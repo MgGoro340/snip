42ed725d0c52de80ef6057d1eeca823c65ff01b0
mggoro <marting@epidataconsulting.com>
2020-11-03 12:36:55 -0300

    nueva modificacion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Snip.
+          Hola Snip 2 !!!!!!!!!!!!!.
         </p>
         <a
           className="App-link"
