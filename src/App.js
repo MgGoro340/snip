15a05e6177f94be9e259326cd2945f9d72fc6698
mggoro <marting@epidataconsulting.com>
2020-11-03 12:49:58 -0300

    amplify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Snip 2 !!!!!!!!!!!!!.
+          Hola Snip !!!!! amplify !!!!!!!.
         </p>
         <a
           className="App-link"
